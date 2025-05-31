b8dc6436f903c1ef2f052ba83212fae970ed13f4
Nada Berliani Putri <161399638+frnadaln@users.noreply.github.com>
2025-05-31 22:54:31 +0700

    Test Suite for Upload Image and Download Design

---
+++ Testing Canva.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testing Canva</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b39534c4-f8c3-44a5-a3a9-818a09499279</testSuiteGuid>
+   <testCaseLink>
+      <guid>89b980ae-4717-4abc-895d-6233d459e053</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testing Canva/mengunduh</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>4e47fe44-9cee-40a5-ae16-d7f1f33860a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testing Canva/gambar</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
